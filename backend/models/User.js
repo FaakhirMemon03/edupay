3bd5a620352f9bdccb67a1873cf08cd97fc06ddb
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-05-20 12:09:20 +0500

    feat: create User mongoose model with password hashing and verification methods

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["admin", "teacher", "parent"],
+    default: "parent"
+  },
+  schoolId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "School"
+  }
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
